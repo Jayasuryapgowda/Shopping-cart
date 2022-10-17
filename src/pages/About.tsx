9dad0e03b5421c7b3ae296a9b7b2c000aeb5b5af
Jayasurya P Gowda <jayasuryagowdap.jpg@gmail.com>
2022-10-17 23:04:59 +0530

    Initial commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,4 @@
+export function About(){
+    return (
+    <h2>This is a website created to demonstrate a shopping cart </h2>
+)}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+export function Home(){
+    return (
+    <h1>Welcome to Jayasurya's Store</h1>
+    
+    
+)}
